3b66fdf426d04ff05fa4eaa66409530e2d7c11d8
최승원 <tmd1568@gmail.com>
2022-09-18 10:45:08 +0900

    바닐라JS로 크롬 앱 만들기
    2-3. const and let까지 공부함.

---
+++ app.js
@@ -0,0 +1,12 @@
+const a = 100;
+const b = 2;
+const myName = "nico";
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log("hello " + myName);
+
+myName = "nicolas";
+
+console.log("your new name is " + myName);
