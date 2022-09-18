5a3c51af87344440d9e2462c947b7428c7876837
최승원 <tmd1568@gmail.com>
2022-09-18 23:01:44 +0900

    바닐라JS로 크롬 앱 만들기
    2-6. Booleans, Arrays, Objects까지 공부함.

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
-const a = 100;
-const b = 2;
-const myName = "nico";
+// 2 - 6 Objects
 
-console.log(a + b);
-console.log(a * b);
-console.log(a / b);
-console.log("hello " + myName);
-
-myName = "nicolas";
-
-console.log("your new name is " + myName);
+const player = {
+    name: "nico",
+    points: 100,
+    fat: true,
+};
+//name만 따로 
+console.log(player);
+console.log(player.name);
+console.log(player["name"])
+//fat 수정하기
+console.log(player.fat)
+player.fat = false;
+console.log(player)
+//lastName 추가하기
+player.lastName = "potato";
+console.log(player)
+// points 숫자 더하기
+player.points = player.points + 100;
+console.log(player)
