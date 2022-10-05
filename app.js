d0549151d53a0b4f2dad9b427051683d3c4d2f50
최승원 <tmd1568@gmail.com>
2022-10-05 19:28:30 +0900

    바닐라JS로 크롬 앱 만들기
    3-3. Events
    3-4. Events part Two
    3-5. More Events

--- app.js
+++ app.js
@@ -1,2 +1,40 @@
-const title = document.querySelector(".hello h1")
+const title = document.querySelector(".hello:first-child h1");
 
+console.dir(title);
+
+function handleTitleClick(){
+    title.style.color = "blue";
+}
+
+function handleMouseEnter(){
+    title.innerText = "Mouse is here!";
+}
+
+function handleMouseLeave(){
+    title.innerText = "Mouse is gone!";
+}
+
+function handleWindowResize(){
+    document.body.style.backgroundColor = "tomato";
+}
+
+function handleWindowCopy(){
+    alert("copier!!!!");
+}
+
+function handleWindowOffline(){
+    alert("SOS no WIFI!!");
+}
+
+function handleWindowOnline(){
+    alert("Oooh good~");
+}
+ 
+title.addEventListener("click", handleTitleClick);
+title.addEventListener("mouseenter", handleMouseEnter);
+title.addEventListener("mouseleave", handleMouseLeave);
+
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy", handleWindowCopy);
+window.addEventListener("offline", handleWindowOffline);
+window.addEventListener("online", handleWindowOnline);
