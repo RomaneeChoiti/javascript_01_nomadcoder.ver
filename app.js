1a835e1a7c5b4153c1560d698d027d37041dcc29
최승원 <tmd1568@gmail.com>
2022-10-06 15:22:48 +0900

    바닐라JS로 크롬 앱 만들기
    3-6. CSS in Javascript
    3-7. CSS in Javascript part Two
    3-8. CSS in Javascript part Three

--- app.js
+++ app.js
@@ -1,40 +1,7 @@
-const title = document.querySelector(".hello:first-child h1");
-
-console.dir(title);
+const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick(){
-    title.style.color = "blue";
+    h1.classList.toggle("clicked");
 }
 
-function handleMouseEnter(){
-    title.innerText = "Mouse is here!";
-}
-
-function handleMouseLeave(){
-    title.innerText = "Mouse is gone!";
-}
-
-function handleWindowResize(){
-    document.body.style.backgroundColor = "tomato";
-}
-
-function handleWindowCopy(){
-    alert("copier!!!!");
-}
-
-function handleWindowOffline(){
-    alert("SOS no WIFI!!");
-}
-
-function handleWindowOnline(){
-    alert("Oooh good~");
-}
- 
-title.addEventListener("click", handleTitleClick);
-title.addEventListener("mouseenter", handleMouseEnter);
-title.addEventListener("mouseleave", handleMouseLeave);
-
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
+h1.addEventListener("click", handleTitleClick);
