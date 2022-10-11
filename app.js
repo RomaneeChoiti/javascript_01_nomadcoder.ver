055035587d00d1b69be97b3851f96a4832cf0f95
최승원 <tmd1568@gmail.com>
2022-10-11 16:41:14 +0900

    바닐라JS로 크롬 앱 만들기
    4-4. Getting Username

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
+const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const loginForm = document.querySelector("#login-form")
-const link = document.querySelector("a")
+const greeting = document.querySelector("#greeting");
 
+const HIDDEN_CLASSNAME = "hidden";
 
-function onLoginSubmit(event){
+function onLoginSubmit(event) {
     event.preventDefault();
-    console.log(loginInput.value);
-}
-function handleLinkClick(event){
-    event.preventDefault();
-    console.log(event)
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
-link.addEventListener("click", handleLinkClick);
