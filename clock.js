b4954f391ceec56c23ee4bbe4b39c68712717ce0
최승원 <tmd1568@gmail.com>
2022-10-19 13:07:19 +0900

    바닐라JS로 크롬 앱 만들기
    #5.2 #5.2 PadStart
    padstart - 00:00:00의 구조를 만듬

--- clock.js
+++ clock.js
@@ -2,7 +2,10 @@ const clock = document.querySelector("h2#clock");
 
 function getClock() {
     const data = new Date()
-    clock.innerText = `${data.getHours()}:${data.getMinutes()}:${data.getSeconds()}`;
+    const hours = String(data.getHours()).padStart(2,"0");
+    const minutes = String(data.getMinutes()).padStart(2,"0");
+    const seconds = String(data.getSeconds()).padStart(2,"0");
+    clock.innerText = `${hours}:${minutes}:${seconds}`;
 }
 
 getClock();
