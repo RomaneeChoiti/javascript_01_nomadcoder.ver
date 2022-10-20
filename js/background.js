d2aa21e92275e3e44949805ee7cccb8f7290ea85
최승원 <tmd1568@gmail.com>
2022-10-20 13:50:13 +0900

    바닐라JS로 크롬 앱 만들기
    #6.1 Background
    3가지 다른 랜덤 background img

---
+++ js/background.js
@@ -0,0 +1,11 @@
+const images = ["0.jpeg", "1.jpeg", "2.jpeg",];
+
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+
+const bgImg = document.createElement("img");
+
+bgImg.src = `img/${chosenImage}`;
+
+console.log(bgImg);
+
+document.body.appendChild(bgImg);
